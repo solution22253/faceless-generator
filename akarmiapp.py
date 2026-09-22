786f35bf272891412cb2b4174ba51a64fc245944
Faceless-Generator <solutionmarketing.expert@gmail.com>
2026-09-23 00:00:36 +0300

    Update akarmiapp.py

--- akarmiapp.py
+++ akarmiapp.py
@@ -39,6 +39,7 @@ def init_db():
     c.execute("""
         INSERT OR IGNORE INTO users (email, license_code, credits)
         VALUES ('teszt@gmail.com', 'PROFI2026', 100)
+        VALUES ('teszt2@gmail.com', 'PROFI2026', 100)
     """)
     conn.commit()
     conn.close()
